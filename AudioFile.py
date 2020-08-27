b1f1f76d0b1af43b96eddbc806bbd3c3534891f7
Mike Aleksa <mtaleksa1@gmail.com>
2020-08-27 13:43:08 -0400

    add 'maintain_length' to mix, add 'trim_tail' to conv_reverb

--- AudioFile.py
+++ AudioFile.py
@@ -12,12 +12,13 @@ import soundfile as sf
 
 class AudioFile:
     """
-    A class for representing audio files.
+    A class for representing and modifying audio files.
     """
 
     def __init__(self, path: Union[str, Path] = None):
         """
         Initialize an audio file. Load file if path is provided.
+
         :param path: path to audio file.
         """
         self.samples: np.ndarray = np.array([])
@@ -45,6 +46,7 @@ class AudioFile:
     def copy(self) -> AudioFile:
         """
         Return a copy of the AudioFile.
+
         :return: a copy of the current object.
         """
         return deepcopy(self)
@@ -52,6 +54,7 @@ class AudioFile:
     def load(self, path: Union[str, Path]):
         """
         Load an audio file.
+
         :param path: path to audio file.
         """
         path = Path(path)
@@ -60,9 +63,13 @@ class AudioFile:
         self.samples, self.sr = librosa.load(path=path, sr=None)
         self.filename = path.name
 
-    def save(self, output_path: Union[str, Path], filename: str = None, subtype: str = 'PCM_16'):
+    def save(self,
+             output_path: Union[str, Path],
+             filename: str = None,
+             subtype: str = 'PCM_16'):
         """
         Save the current audio samples to a new file.
+
         :param output_path: output path for audio samples.
         :param filename: filename for output - will use the original filename of the audio file by default.
         :param subtype: soundfile subtype - 16-bit PCM by default.
@@ -78,15 +85,17 @@ class AudioFile:
     def resample(self, new_sr: int):
         """
         Resample to new sample rate.
+
         :param new_sr: target sample rate.
         """
         self.samples = librosa.resample(self.samples, self.sr, new_sr)
         self.sr = new_sr
         return self
 
-    def scale_amplitude(self, scale: float):
+    def scale(self, scale: float):
         """
         Scale amplitudes by a constant.
+
         :param scale: constant used to scale audio
         """
         self.samples = self.samples * scale
@@ -99,15 +108,16 @@ class AudioFile:
         self.samples = librosa.util.normalize(self.samples, axis=-1)
         return self
 
-    def clip_amplitude(self, clip_amount: float = 2.0):
+    def clip(self, clip_amount: float = 2.0):
         """
         Digitally clip audio samples by scaling beyond [-1., 1.] and clipping to [-1., 1.].
+
         :param clip_amount: A number > 1.0 used to scale the normalized audio samples.
         """
         peak = min(1.0, self.peak)
-        self.normalize().scale_amplitude(clip_amount)
+        self.normalize().scale(clip_amount)
         np.clip(self.samples, -1.0, 1.0, out=self.samples)
-        self.scale_amplitude(peak / clip_amount)
+        self.scale(peak / clip_amount)
         return self
 
     def add_silence(self,
@@ -117,6 +127,7 @@ class AudioFile:
                     sec_after: float = 0.0):
         """
         Add silence to the end of the audio samples.
+
         :param samples_before: number of samples of silence to add to beginning of the audio data. Takes precedence over sec_before if set.
         :param samples_after: number of samples of silence to add to end of the audio data. Takes precedence over sec_after if set.
         :param sec_before: seconds of silence to add the beginning of the audio data.
@@ -132,16 +143,10 @@ class AudioFile:
         self.samples = np.concatenate((self.samples, silence_after))
         return self
 
-    # def pitch_shift(self, n_steps: float):
-    #     """
-    #     Shift pitch of audio data.
-    #     :param n_steps: how many steps to shift
-    #     """
-    #     self.samples = librosa.effects.pitch_shift(self.samples, self.sr, n_steps=n_steps)
-
     def varispeed(self, rate: float):
         """
         Stretch time/pitch of audio data by increasing/decreasing playback speed.
+
         :param rate: Stretch factor. If rate > 1, then the signal is sped up. If rate < 1, then the signal is slowed down.
         """
         old_sr = self.sr
@@ -149,14 +154,20 @@ class AudioFile:
         self.resample(old_sr)
         return self
 
-    def mix(self, audio: AudioFile, relative_start: float = 1.0):
+    def mix(self,
+            audio: AudioFile,
+            relative_start: float = 1.0,
+            maintain_length: bool = False):
         """
         Combine two audio files.
+
         :param audio: an AudioFile to add to the current AudioFile.
         :param relative_start: the start position of audio relative to the current audio - i.e. 0.5 adds audio starting
         in the middle of the current audio.
+        :param: maintain_length: if true, mixed audio will be trimmed to maintain the same length as the original audio
         """
         assert(self.sr == audio.sr)
+        shape = self.samples.shape
         audio = audio.copy()
 
         # delay the start of the new audio
@@ -169,38 +180,51 @@ class AudioFile:
 
         # combine signals
         self.samples = self.samples + audio.samples
+        if maintain_length:
+            self.samples.resize(shape)
         return self
 
-    def lpf(self, order, cutoff):
+    def lpf(self, cutoff: float, order: int = 1):
         """
         Low-pass filter.
+
         https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html#scipy.signal.butter
-        :param order: the order of the filter.
+
         :param cutoff: cutoff frequency in Hz.
+        :param order: the order of the filter.
         """
         sos = signal.butter(order, cutoff, btype='low', analog=False, output='sos', fs=self.sr)
         filtered = signal.sosfilt(sos, self.samples)
         self.samples = filtered
         return self
 
-    def conv_reverb(self, ir: AudioFile, dry_mix: float = 1.0, wet_mix: float = 1.0, predelay: float = 0.0):
+    def conv_reverb(self,
+                    ir: AudioFile,
+                    dry_mix: float = 1.0,
+                    wet_mix: float = 1.0,
+                    predelay: float = 0.0,
+                    trim_tail: bool = True):
         """
         Convolution reverb.
+
         :param ir: AudioFile containing impulse response.
         :param dry_mix: scale of dry audio.
-        :param mix: scale of wet audio.
+        :param wet_mix: scale of wet audio.
         :param predelay: predelay added to wet audio.
+        :param trim_tail: if True, will trim tail of new audio to maintain length of original audio.
         """
         ir = ir.copy()
         if ir.sr != self.sr:
             ir.resample(self.sr)
 
         convolution = self.copy()
-        convolution.samples = signal.convolve(self.samples, ir.samples)
+        convolution.samples = signal.convolve(self.samples, ir.samples, mode='full')
+        if trim_tail:
+            convolution.samples.resize(self.samples.shape)
 
         if predelay > 0.0:
             convolution.add_silence(sec_before=predelay/1000)
 
-        self.scale_amplitude(dry_mix).mix(convolution.scale_amplitude(wet_mix), relative_start=0.0)
-        self.clip_amplitude(clip_amount=1.0)
+        self.scale(dry_mix).mix(convolution.scale(wet_mix), relative_start=0.0)
+        self.clip(clip_amount=1.0)
         return self
